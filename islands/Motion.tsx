cdeffe528edb5f39350a50909112ae49c92d5410
Spike <l-yanlei@hotmail.com>
2023-04-20 13:41:16 +0800

    :recycle: request motion permission

--- islands/Motion.tsx
+++ islands/Motion.tsx
@@ -12,18 +12,42 @@ export default function Counter() {
     deviceOrientation: {}
   });
 
-  useEffect(() => {
-    window.addEventListener("devicemotion", ({ acceleration, accelerationIncludingGravity, rotationRate }) => {
-      setInfo((state) => { return { ...state, acceleration, accelerationIncludingGravity, rotationRate } });
-    }, true);
+  const requestDeviceMotionPermission = () => {
+    if (typeof DeviceMotionEvent.requestPermission === 'function') {
+      DeviceMotionEvent.requestPermission()
+        .then(permissionState => {
+          if (permissionState === 'granted') {
+            console.log("granted")
+            window.addEventListener("devicemotion", ({ acceleration, accelerationIncludingGravity, rotationRate }) => {
+              setInfo((state) => { return { ...state, acceleration, accelerationIncludingGravity, rotationRate } });
+            }, true);
+          }
+        })
+        .catch(console.error);
+    }
+  }
 
-    window.addEventListener("deviceorientation", (event) => {
-      setInfo((state) => { return { ...state, deviceOrientation: event } });
-    });
-  }, []);
+  const requestDeviceorientationPermission = () => {
+    if (typeof DeviceOrientationEvent.requestPermission === 'function') {
+      DeviceOrientationEvent.requestPermission()
+        .then(permissionState => {
+          if (permissionState === 'granted') {
+            window.addEventListener("deviceorientation", (event) => {
+              setInfo((state) => { return { ...state, deviceOrientation: event } });
+            });
+          }
+        })
+        .catch(console.error);
+    }
+  }
+
 
   return (
     <div class="flex-column gap-2 w-full">
+      <div>
+        <button onClick={requestDeviceMotionPermission}>requestDeviceMotionPermission</button>
+        <button onClick={requestDeviceorientationPermission} class="ml-4">requestDeviceorientationPermission</button>
+      </div>
       <fieldset>
         <legend>acceleration</legend>
         <p>X: {info.acceleration.x}</p>
